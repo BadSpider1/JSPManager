86b07c1c0057ca071248c38069992db77c15d781
BadSpider1 <badspider5@gmail.com>
2025-01-02 01:02:28 +0100

    Push project files

---
+++ JSPManager/Config.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.Json;
+
+namespace JSPManager
+{
+    public class Config
+    {
+        public string token { get; set; } = "insert-your-token";
+    }
+    public class ConfigUtils
+    {
+        public static async Task CreateConfig()
+        {
+            try
+            {
+                Console.WriteLine("Config file not found. Creating one ...");
+                var DefConf = new Config();
+                var json = JsonSerializer.Serialize(DefConf);
+                await File.WriteAllTextAsync("config.json", json);
+                Console.WriteLine("Configuration file created" + json);
+            }
+            catch (Exception e) {
+                Console.Write("An error has occoured while trying to create configuration file" + e.Message);
+            }
+        }
+    }
+}
---
+++ JSPManager/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.Json;
+using Discord;
+using Discord.WebSocket;
+
+namespace JSPManager {
+    public class Program
+    {
+        private static DiscordSocketClient _client;
+        public static async Task Main()
+        {
+
+            Logging.LogInfo("JSP Manager version 0.1 starting ...");
+
+            if (!File.Exists("config.json"))
+            {
+                ConfigUtils.CreateConfig();
+                return;
+            }
+            string _config = await File.ReadAllTextAsync("config.json");
+            var config = JsonSerializer.Deserialize<Config>(_config);
+            Logging.LogInfo("Config successfully parsed");
+
+            _client = new DiscordSocketClient();
+            _client.Log += Logging.Log;
+            await _client.LoginAsync(TokenType.Bot, config.token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
